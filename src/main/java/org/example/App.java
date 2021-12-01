b955b5f8400172915a7e1c176f2d4bc7de8d6d1d
darwinmwstafa <darwin.mustafa92@gmail.com>
2021-12-01 14:13:35 +0100

    3 V

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Int 1" );
+        System.out.println( "Hello World! Int 1");
+        System.out.println("Hello2");
     }
 }
